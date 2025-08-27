77d3f9a4f8b9e2dd5f95ef14144254c868b55ec4
Abdullahi Osman <abdalla-92@hotmail.com>
2025-08-27 18:31:04 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,7 +48,7 @@ if ingredient_list:
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
             values ('""" + ingredients_string + """', '"""+ name_on_order +"""' )"""
 
-    #st.write(my_insert_stmt)
+    st.write(my_insert_stmt)
 
     time_to_insert = st.button("Submit Order")
     if time_to_insert:
